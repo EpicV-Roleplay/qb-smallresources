80c92785c77a3d6a8bb7916713c32ce99adb6fb0
EpicV-Roleplay <102457093+EpicV-Roleplay@users.noreply.github.com>
2022-04-10 00:52:26 +0200

    Update chillzone.lua

--- client/chillzone.lua
+++ client/chillzone.lua
@@ -1,15 +1,15 @@
 local chillzone = false
 
 Config.Points = {
-    vector3(0.0.0)
+    vector3(200.47, -921.7, 30.69)
 }
 
 Citizen.CreateThread(function()
     while true do
         for k,v in ipairs(Config.Points) do
             local distance = #(GetEntityCoords(PlayerPedId()) - v)
-            if distance < 20 than
-                if chillzone == false than
+            if distance < 20 then
+                if chillzone == false then
                     chillzone = true
                     QBCore.Functions.Notify('Du hast eine ChillZone betreten!', 'error', 3000)
                     break
